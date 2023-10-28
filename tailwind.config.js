283d85631c6fbe7762a5d1b8038b5ace271e377b
Arthur Williams <arthurwiliams86@gmail.com>
2023-10-28 21:31:32 +0300

    pref: Extract `colors` key out of `extends` key

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,13 @@ module.exports = {
     fontFamily: {
       sans: ['Roboto', ...DEFAULT_THEME.fontFamily.sans]
     },
-
-    extend: {
-      colors: {
-        'tomato': 'hsl(4, 100%, 67%)',
-        'dark-slate-grey': 'hsl(234, 29%, 20%)',
-        'charcoal-grey': 'hsl(235, 18%, 26%)',
-        'grey': 'hsl(231, 7%, 60%)',
-        'white': 'hsl(0, 0%, 100%)'
-      }
-    },
+    colors: {
+      'tomato': 'hsl(4, 100%, 67%)',
+      'dark-slate-grey': 'hsl(234, 29%, 20%)',
+      'charcoal-grey': 'hsl(235, 18%, 26%)',
+      'grey': 'hsl(231, 7%, 60%)',
+      'white': 'hsl(0, 0%, 100%)'
+    }
   },
   plugins: [],
 }
