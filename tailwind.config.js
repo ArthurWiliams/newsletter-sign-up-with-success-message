1cafcfc816fc3dd27671ddce990af78ad9e84991
Arthur Williams <arthurwiliams86@gmail.com>
2023-11-16 00:44:48 +0300

    chore: Add lighter tint

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
       sans: ['Roboto', ...DEFAULT_THEME.fontFamily.sans]
     },
     colors: {
-      'tomato': 'hsl(4, 100%, 67%)',
+      'tomato': {
+        100: 'hsl(5, 100%, 95%)',
+        800: 'hsl(4, 100%, 67%)'
+      },
       'dark-slate-grey': 'hsl(234, 29%, 20%)',
       'charcoal-grey': 'hsl(235, 18%, 26%)',
       'grey': 'hsl(231, 7%, 60%)',
